8502f4540c45ced2cb87407b65b4859c8ee8e7f9
Ura <alexanoffura1@mail.ru>
2024-06-11 04:53:35 +0300

    add stats

--- main/src/test/java/com/example/main/MainApplicationTests.java
+++ main/src/test/java/com/example/main/MainApplicationTests.java
@@ -1,13 +1,12 @@
 package com.example.main;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class MainApplicationTests {
 
-    @Test
-    void contextLoads() {
-    }
+//    @Test
+//    void contextLoads() {
+//    }
 
 }

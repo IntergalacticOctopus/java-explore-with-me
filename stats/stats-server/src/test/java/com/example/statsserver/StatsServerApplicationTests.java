4f1585ccb3e775e46ad8138ca367cc3509c22d2b
Ura <alexanoffura1@mail.ru>
2024-06-15 02:55:57 +0300

    add stats

--- stats/stats-server/src/test/java/com/example/statsserver/StatsServerApplicationTests.java
+++ stats/stats-server/src/test/java/com/example/statsserver/StatsServerApplicationTests.java
@@ -1,12 +1,13 @@
 package com.example.statsserver;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class StatsServerApplicationTests {
 
-//    @Test
-//    void contextLoads() {
-//    }
+    @Test
+    void contextLoads() {
+    }
 
 }

e5899fb26376eb73813426bbeffb5a110fb163f3
Ura <alexanoffura1@mail.ru>
2024-06-15 02:55:57 +0300

    add stats

--- stats/client/src/main/java/ru/practicum/client/StatClient.java
+++ stats/client/src/main/java/ru/practicum/client/StatClient.java
@@ -1,43 +1,70 @@
 package ru.practicum.client;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.web.client.RestTemplateBuilder;
-import org.springframework.http.ResponseEntity;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.*;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.util.DefaultUriBuilderFactory;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
 import ru.practicum.HitDto;
+import ru.practicum.StatDto;
 
 import java.time.LocalDateTime;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 
-@Service
-public class StatClient extends BaseClient {
+@Component
+public class StatClient {
 
     @Autowired
-    public StatClient(@Value("${stat-server.url}") String serverUrl, RestTemplateBuilder builder) {
-        super(
-                builder
-                        .uriTemplateHandler(new DefaultUriBuilderFactory(serverUrl))
-                        .requestFactory(HttpComponentsClientHttpRequestFactory::new)
-                        .build()
-        );
+    public StatClient(@Value("${stat-server.url}") String serverUrl) {
+        this.restTemplate = new RestTemplate();
+        this.serverUrl = serverUrl;
     }
 
-    public ResponseEntity<Object> saveHit(HitDto hitDto) {
-        return post("/hit", hitDto);
+    private final RestTemplate restTemplate;
+    private final String serverUrl;
+
+    public void saveHit(HitDto hitDto) {
+        try {
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+            HttpEntity<HitDto> entity = new HttpEntity<>(hitDto, headers);
+            ResponseEntity<Void> response = restTemplate.postForEntity(serverUrl + "/hit", entity, Void.class);
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new RuntimeException("Failed to save hit: " + response.getStatusCode());
+            }
+        } catch (Exception e) {
+            System.err.println("Error saving hit: " + e.getMessage());
+        }
     }
 
-    public ResponseEntity<Object> getStats(LocalDateTime start, LocalDateTime end, Boolean unique, List<String> uris) {
+    public List<StatDto> getStats(LocalDateTime start, LocalDateTime end, Boolean unique, List<String> uris) {
         Map<String, Object> parameters = Map.of(
                 "start", start,
                 "end", end,
                 "unique", unique,
                 "uris", uris
         );
-        return get("/stats?start={start}&end={end}&unique={unique}&uris={uris}", parameters);
+
+        try {
+            UriComponentsBuilder builder = UriComponentsBuilder.fromHttpUrl(serverUrl + "/stats")
+                    .queryParam("start", start)
+                    .queryParam("end", end)
+                    .queryParam("unique", unique)
+                    .queryParam("uris", String.join(",", uris));
+
+            ResponseEntity<List<StatDto>> response = restTemplate.exchange(builder.toUriString(), HttpMethod.GET, null, new ParameterizedTypeReference<List<StatDto>>() {});
+            if (!response.getStatusCode().is2xxSuccessful()) {
+                throw new RuntimeException("Failed to get stats: " + response.getStatusCode());
+            }
+            return response.getBody();
+        } catch (Exception e) {
+            System.err.println("Error getting stats: " + e.getMessage());
+            return Collections.emptyList();
+        }
     }
 }

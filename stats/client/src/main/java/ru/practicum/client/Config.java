e066071a586fa1c998cf987e0fa5a0e7f5299799
AlexanoffUra <131712695+IntergalacticOctopus@users.noreply.github.com>
2024-07-06 09:37:03 +0300

    add main

--- stats/client/src/main/java/ru/practicum/client/Config.java
+++ stats/client/src/main/java/ru/practicum/client/Config.java
@@ -2,7 +2,6 @@ package ru.practicum.client;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 
 @Configuration

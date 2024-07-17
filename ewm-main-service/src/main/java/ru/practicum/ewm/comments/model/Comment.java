e2a6fed51f989120324d7e6af00535859f09d6e5
AlexanoffUra <131712695+IntergalacticOctopus@users.noreply.github.com>
2024-07-18 02:17:51 +0300

    add comments

--- ewm-main-service/src/main/java/ru/practicum/ewm/comments/model/Comment.java
+++ ewm-main-service/src/main/java/ru/practicum/ewm/comments/model/Comment.java
@@ -4,7 +4,6 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.util.Lazy;
 import ru.practicum.ewm.events.model.Event;
 import ru.practicum.ewm.user.model.User;
 
